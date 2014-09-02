e70d68f04979ce58e2f07bc864bcf451da78afd2
Ditofry <brandon@cirrously.com>
2014-09-02 01:44:41 -0600

    entity_code_redesign
    Changes architecture of EntityCombatant and adds EntityHuman, which is identical to EntityCombatant save for the fact that it has keybindings.  The control flow of an entities actions has been broken out in the following way:  Both entities have methods that correspond to actions, those methods govern properties that describe the nature of the actions (e.g. walkingDirection: 'left'), the update function automatically makes the entity 'go' based on the properties it needs to observe.  This PR also implements a working prototype of the multiplayer server, and adds socket messaging support for EntityHuman and EntityCombatant.

--- middlewares/multiplayer_server.js
+++ middlewares/multiplayer_server.js
@@ -1,12 +1,23 @@
 var scheme   = "ws://"; // No persistence == no security needed... for now
 var uri      = scheme + window.document.location.host + "/";
 var ws       = new WebSocket(uri);
+var netPlayers = {};
+// var localPlayer = ig.game.getEntitiesByType('Human')[0].playerId;
+var localPlayer = null;
 
 ws.onmessage = function(message) {
   var data = JSON.parse(message.data);
+
+  // Add netplayers
   if ( data.action == 'playerJoined' ) {
-    ig.game.spawnEntity(EntityCombatant,50,50);
+    if ( data.data.playerId == localPlayer ) { return; }
+    entity = ig.game.spawnEntity(EntityCombatant,50,50, {playerId: data.data.playerId});
+    netPlayers[data.data.playerId] = entity;
+  } else {
+    // Invoke the paired method
+    netPlayers[data.playerId][data.action]();
   }
+
 };
 
 var networkAction = {
