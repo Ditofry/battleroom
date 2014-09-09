0064d62a60fa38ef42fbe7b91e4c9a4d5fd995a3
Ditofry <brandon@cirrously.com>
2014-09-09 10:02:17 -0600

    multiplayer_server_debug
    This commit ammends some of the wacky socket behavior that was being experienced following the initial implementation of the multiplayer server and the entity re-design.  Players now appear in their proper place in the world for they are joining or being joined (in other words, if you join a game you can now see pre-existing players), When a player dies or disconnects their entity will now be removed from the game.

--- middlewares/multiplayer_server.js
+++ middlewares/multiplayer_server.js
@@ -2,7 +2,6 @@ var scheme   = "ws://"; // No persistence == no security needed... for now
 var uri      = scheme + window.document.location.host + "/";
 var ws       = new WebSocket(uri);
 var netPlayers = {};
-// var localPlayer = ig.game.getEntitiesByType('Human')[0].playerId;
 var localPlayer = null;
 
 ws.onmessage = function(message) {
@@ -10,12 +9,43 @@ ws.onmessage = function(message) {
 
   // Add netplayers
   if ( data.action == 'playerJoined' ) {
-    if ( data.data.playerId == localPlayer ) { return; }
-    entity = ig.game.spawnEntity(EntityCombatant,50,50, {playerId: data.data.playerId});
-    netPlayers[data.data.playerId] = entity;
-  } else {
+    // If local player is doing the joining
+    if ( data.playerId == localPlayer ) {
+      // Contact pre-existing players
+      ws.send(JSON.stringify({action: 'reconoiter', netPlayer: localPlayer }));
+    } else {
+      entity = ig.game.spawnEntity(EntityCombatant,50,50, {playerId: data.playerId});
+      netPlayers[data.playerId] = entity;
+    }
+  }
+
+  if ( data.action == 'reconoiter' ) {
+    // Would be nice if we could send to all BUT sender...
+    if ( data.netPlayer !== localPlayer ) {
+      var human = ig.game.getEntityByName('bee'),
+          posx = human.pos.x,
+          posy = human.pos.y;
+      ws.send(JSON.stringify({action: 'handshake', playerId: localPlayer, posx: posx, posy: posy }));
+    }
+  }
+
+  if ( data.action == 'handshake' ) {
+    if ( data.playerId !== localPlayer ) {
+      entity = ig.game.spawnEntity(EntityCombatant, data.posx, data.posy, {playerId: data.playerId});
+      netPlayers[data.playerId] = entity;
+    }
+  }
+
+  if ( data.action == 'playerLeft' ) {
+    netPlayers[data.playerId].kill();
+  }
+
+  // UGLYUGLYUGLY just for now
+  if ( data.action !== 'handshake' && data.action !== 'reconoiter' && data.action !== 'playerJoined' && data.action !== 'playerLeft' ) {
     // Invoke the paired method
-    netPlayers[data.playerId][data.action]();
+    if ( netPlayers[data.playerId] ) {
+      netPlayers[data.playerId][data.action]();
+    }
   }
 
 };
